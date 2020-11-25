0da58728e13539a5f916b0d6914b6f0180e2c407
Gonçalo Teixeira <goncalo.ac.teixeira@gmail.com>
2020-11-25 01:57:14 +0000

    login style done, need to work on some verifications

--- adov_flutter/lib/screens/login/login.dart
+++ adov_flutter/lib/screens/login/login.dart
@@ -3,6 +3,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import '../../style.dart';
+
 class LoginScreen extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
@@ -18,65 +20,137 @@ class LoginScreenState extends State<LoginScreen> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   bool _success;
   String _userEmail;
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Login"),
-      ),
-      body: Form(
-        key: _formKey,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            TextFormField(
-              controller: _emailController,
-              decoration: const InputDecoration(labelText: 'Email'),
-              validator: (String value) {
-                if (value.isEmpty) {
-                  return 'Please enter some text';
-                }
-                return null;
-              },
-            ),
-            TextFormField(
-              controller: _passwordController,
-              decoration: const InputDecoration(labelText:
-              'Password'),
-              validator: (String value) {
-                if (value.isEmpty) {
-                  return 'Please enter some text';
-                }
-                return null;
-              },
-            ),
-            Container(
-              padding: const EdgeInsets.symmetric(vertical: 16.0),
-              alignment: Alignment.center,
-              child: RaisedButton(
-                onPressed: () async {
-                  if (_formKey.currentState.validate()) {
-                    _signInWithEmailAndPassword();
-                  }
-                },
-                child: const Text('Submit'),
+      key: _scaffoldKey,
+      backgroundColor: MainColor,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Center(
+            child: Text(
+              "Ad\nOv",
+              style: TitleTextStyle.copyWith(
+                color: Colors.white,
+                fontSize: 100,
               ),
             ),
-            Container(
-              alignment: Alignment.center,
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: Text(
-                _success == null
-                    ? ''
-                    : (_success
-                    ? 'Successfully signed in ' + _userEmail
-                    : 'Sign in failed'),
-                style: TextStyle(color: Colors.red),
-              ),
-            )
-          ],
-        ),
+          ),
+          Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 30),
+                  child: TextFormField(
+                    controller: _emailController,
+                    style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 24
+                    ),
+                    decoration: CustomInputDecorator.decorator("Email").copyWith(
+                      fillColor: Colors.grey,
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        borderSide: BorderSide(
+                          color: Colors.grey,
+                          width: 1.5,
+                        ),
+                      ),
+                      labelStyle: TextStyle(
+                          color: Colors.grey
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        borderSide: BorderSide(
+                          color: Colors.grey,
+                          width: 1.5,
+                        ),
+                      ),
+                    ),
+                    validator: (String value) {
+                      if (value.isEmpty) {
+                        return '';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                  child: TextFormField(
+                    controller: _passwordController,
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 24,
+                    ),
+                    enableSuggestions: false,
+                    autocorrect: false,
+                    obscureText: true,
+                    decoration: CustomInputDecorator.decorator("Password").copyWith(
+                      fillColor: Colors.grey,
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        borderSide: BorderSide(
+                          color: Colors.grey,
+                          width: 1.5,
+                        ),
+                      ),
+                      labelStyle: TextStyle(
+                          color: Colors.grey
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        borderSide: BorderSide(
+                          color: Colors.grey,
+                          width: 1.5,
+                        ),
+                      ),
+                    ),
+                    validator: (String value) {
+                      if (value.isEmpty) {
+                        return '';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 16.0),
+                  alignment: Alignment.center,
+                  child: OutlineButton(
+                    onPressed: () async {
+                      FocusScope.of(context).unfocus();
+                      if (_formKey.currentState.validate()) {
+                        _signInWithEmailAndPassword();
+                      }
+                    },
+                    child: Text(
+                      "Login",
+                      style: TitleTextStyle.copyWith(
+                        color: Colors.white,
+                        fontSize: 24,
+                      ),
+                    ),
+                    padding: EdgeInsets.symmetric(vertical: 7, horizontal: 50),
+                    borderSide: BorderSide(
+                      color: Colors.grey,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(26.0),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -93,12 +167,23 @@ class LoginScreenState extends State<LoginScreen> {
         _userEmail = user.email;
       });
 
-      Navigator.pushReplacementNamed(context, HomeScreenRoute);
+      print("email: " + _emailController.text);
+      print("password: " + _passwordController.text);
 
+      final snackBar = SnackBar(content: Text("Successfully Logged In..."));
+      _scaffoldKey.currentState.showSnackBar(snackBar);
+
+      Future.delayed(Duration(seconds: 1), () {
+        Navigator.pushReplacementNamed(context, HomeScreenRoute);
+      });
     } else {
       setState(() {
         _success = false;
       });
+      final snackBar = SnackBar(content: Text("Wrong credentials..."));
+      print("email: " + _emailController.text);
+      print("password: " + _passwordController.text);
+      _scaffoldKey.currentState.showSnackBar(snackBar);
     }
   }
 
